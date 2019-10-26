83f7ac2bff8fa256202525c9518d2b47d48ac0e6
Huy Nguyen <kqhuy.nguyen@gmail.com>
2019-10-26 14:16:17 +0700

    Added links & responsive homepage

--- javascripts/hompage.js
+++ javascripts/hompage.js
@@ -6,3 +6,20 @@ function openNav() {
     a.style.width= "250px";
   }
 }
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      previewRow.innerHTML = previewRow.innerHTML + `
+            <div class="col s4">
+              <img width="100%" class="materialboxed" src=${e.target.result}>
+            </div>
+            `
+    }
+
+    reader.readAsDataURL(input.files[0]);
+
+    document.getElementById('uploadTips').style.display = 'none';
+  }
+}
